0ca800bf973a646411e61e7aaa03d478f020484d
Matt Jackson <me@mattjackson.uk>
2019-08-05 20:31:46 +0100

    Use X-Forwarded-For as remote IP if it is present

--- bowelism/log_streaming/views.py
+++ bowelism/log_streaming/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'index.html', context={
-        'remote_ip': request.META['REMOTE_ADDR']
+        'remote_ip': request.META.get('HTTP_X_FORWARDED_FOR') or request.META['REMOTE_ADDR']
     })
