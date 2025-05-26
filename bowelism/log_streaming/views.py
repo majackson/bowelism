25a0ad2e5a2bf00de9fcb1b445079c36dc718ba9
Matt Jackson <me@mattjackson.uk>
2025-05-26 11:13:23 +0300

    different sections

--- bowelism/log_streaming/views.py
+++ bowelism/log_streaming/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 
 def homepage(request):
+    if 'bowelism' in request.path:
+        content = 'bowelism'
+    elif 'user-manual' in request.path:
+        content = 'user-manual'
+    else:
+        content = 'intro'
+
     return render(request, 'index.html', context={
-        'remote_ip': request.META.get('HTTP_X_FORWARDED_FOR') or request.META['REMOTE_ADDR']
+        'remote_ip': request.META.get('HTTP_X_FORWARDED_FOR') or request.META['REMOTE_ADDR'],
+        'content': content
     })
--- bowelism/urls.py
+++ bowelism/urls.py
@@ -5,5 +5,8 @@ from bowelism.log_streaming import views as log_streaming_views
 
 urlpatterns = [
     re_path(r'^$', log_streaming_views.homepage, name='homepage'),
+    re_path(r'^user-manual', log_streaming_views.homepage, name='homepage'),
+    re_path(r'^bowelism', log_streaming_views.homepage, name='homepage'),
+    re_path(r'^intro', log_streaming_views.homepage, name='homepage'),
     re_path(r'^', include('bowelism.api.urls')),
 ]
